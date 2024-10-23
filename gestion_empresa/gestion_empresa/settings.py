58c5b80c5aeea94330faa91fab9e4d3b5e5caf76
joksanhaniel <joksanhaniel@gmail.com>
2024-10-23 15:04:04 -0600

    bootstrap

--- gestion_empresa/gestion_empresa/settings.py
+++ gestion_empresa/gestion_empresa/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
      'gestion_rrhh',
+      'crispy_forms',
+      'crispy_bootstrap5',
 ]
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
