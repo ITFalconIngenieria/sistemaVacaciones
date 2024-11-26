8dcf7b91fed590b93960244f3362121a917e553c
joksanhaniel <joksanhaniel@gmail.com>
2024-11-26 16:28:23 -0600

    code cleaned

--- gestion_empresa/gestion_rrhh/forms.py
+++ gestion_empresa/gestion_rrhh/forms.py
@@ -54,12 +54,10 @@ class SolicitudForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        # Si el formulario tiene una instancia existente (editar solicitud)
         if self.instance and self.instance.pk:
             self.fields['tipo'].widget.attrs['readonly'] = True  
             self.fields['tipo'].widget.attrs['style'] = 'pointer-events: none;'
 
-              # Preformatear fechas para que el widget DateTimeInput las cargue correctamente
             if self.instance.fecha_inicio:
                 self.initial['fecha_inicio'] = self.instance.fecha_inicio.strftime('%Y-%m-%dT%H:%M')
             if self.instance.fecha_fin:
@@ -70,15 +68,12 @@ class SolicitudForm(forms.ModelForm):
         fecha_inicio = cleaned_data.get('fecha_inicio')
         fecha_fin = cleaned_data.get('fecha_fin')
 
-        # Validar que las fechas estén presentes
         if not fecha_inicio or not fecha_fin:
             raise ValidationError("Las fechas de inicio y fin son obligatorias.")
 
-        # Validar que la fecha de inicio no sea posterior a la fecha de fin
         if fecha_inicio > fecha_fin:
             raise ValidationError("La fecha de inicio no puede ser posterior a la fecha de fin.")
 
-        # Validar que la fecha de inicio no sea anterior a la fecha actual
         fecha_actual = timezone.now()
         if fecha_inicio.date() < fecha_actual.date():
             raise ValidationError("La fecha de inicio no puede ser anterior a la fecha actual.")
@@ -143,7 +138,6 @@ class RegistrarHorasForm(forms.ModelForm):
         descripcion = cleaned_data.get('descripcion')
         fecha_actual = timezone.now()
 
-
         if (numero_proyecto is None or numero_proyecto == 0) and not descripcion:
             raise forms.ValidationError(
                 "Debe llenar la descripción porque el número de proyecto está vacío o es 0."
@@ -163,9 +157,7 @@ class RegistroHorasFilterForm(forms.Form):
         ('A', 'Aprobada'),
         ('R', 'Rechazada'),
     )
-
     estado = forms.ChoiceField(choices=[('', 'Todos')] + list(ESTADOS), required=False, label="Estado")
-    
     usuario = forms.ModelChoiceField(queryset=Usuario.objects.none(), required=False, label="Usuario")
 
     def __init__(self, *args, **kwargs):
@@ -176,8 +168,6 @@ class RegistroHorasFilterForm(forms.Form):
             self.fields['usuario'].queryset = Usuario.objects.filter(jefe=self.user) | Usuario.objects.filter(id=self.user.id)
         else:
             del self.fields['usuario']
-
-
 
 
 class AjusteVacacionesForm(forms.ModelForm):
@@ -198,40 +188,6 @@ class AjusteVacacionesForm(forms.ModelForm):
                 'style': 'width: 50%;', 
             })
         }
-
-# class IncapacidadForm(forms.ModelForm):
-#     class Meta:
-#         model = Incapacidad
-#         fields = ['fecha_inicio', 'fecha_fin', 'archivo_adjunto', 'descripcion']
-#         widgets = {
-#             'fecha_inicio': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
-#             'fecha_fin': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
-#             'descripcion': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
-#             'archivo_adjunto': forms.FileInput(attrs={'class': 'form-control'}),
-#         }
-
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
-#         if self.instance.pk:
-#             if self.instance.fecha_inicio:
-#                 self.initial['fecha_inicio'] = self.instance.fecha_inicio
-#             if self.instance.fecha_fin:
-#                 self.initial['fecha_fin'] = self.instance.fecha_fin
-
-#     def clean(self):
-#         cleaned_data = super().clean()
-#         fecha_inicio = cleaned_data.get('fecha_inicio')
-#         fecha_fin = cleaned_data.get('fecha_fin')
-
-#         if not fecha_inicio or not fecha_fin:
-#             raise forms.ValidationError("Ambas fechas son obligatorias.")
-
-#         # Validar que las fechas estén en días completos y en el rango permitido
-#         diferencia_dias = (fecha_fin - fecha_inicio).days + 1
-#         if diferencia_dias < 1 or diferencia_dias > 3:
-#             raise forms.ValidationError("El rango de días debe ser entre 1 y 3 días completos.")
-
-#         return cleaned_data
 
 
 class IncapacidadForm(forms.ModelForm):
@@ -259,7 +215,6 @@ class IncapacidadForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         if self.instance and self.instance.pk:
-            # Convertir las fechas al formato correcto para el input type="date"
             if self.instance.fecha_inicio:
                 self.initial['fecha_inicio'] = self.instance.fecha_inicio.strftime('%Y-%m-%d')
             if self.instance.fecha_fin:
@@ -273,7 +228,6 @@ class IncapacidadForm(forms.ModelForm):
         if not fecha_inicio or not fecha_fin:
             raise forms.ValidationError("Ambas fechas son obligatorias.")
 
-        # Validar que las fechas estén en días completos y en el rango permitido
         diferencia_dias = (fecha_fin - fecha_inicio).days + 1
         if diferencia_dias < 1 or diferencia_dias > 3:
             raise forms.ValidationError("El rango de días debe ser entre 1 y 3 días completos.")
