07a3384fd7e0931e0398dc9605ed092ba9aca957
joksanhaniel <joksanhaniel@gmail.com>
2025-04-30 15:04:51 -0600

    modulos admin

--- gestion_empresa/gestion_rrhh/admin.py
+++ gestion_empresa/gestion_rrhh/admin.py
@@ -1,7 +1,39 @@
-from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Usuario, FeriadoNacional,  HorasCompensatoriasDescanso
 from .forms import UsuarioCreationForm, UsuarioChangeForm
+from django.contrib import admin
+from django.apps import apps
+from django.contrib.admin.sites import AlreadyRegistered
+from .models import (
+    Usuario, 
+    FeriadoNacional, 
+      HorasCompensatoriasDescanso,
+    Departamento,
+    Solicitud,
+    RegistroHoras,
+    HistorialVacaciones,
+    AjusteVacaciones,
+    Incapacidad,
+    Licencia,
+    ConversionVacacionesHoras,
+    HorasCompensatoriasSieteDias,
+    CodigoRestablecimiento,
+    RegistroHorasOdoo
+)
+
+
+admin.site.register(Departamento)
+admin.site.register(Solicitud)
+admin.site.register(RegistroHoras)
+admin.site.register(HistorialVacaciones)
+admin.site.register(AjusteVacaciones)
+admin.site.register(Incapacidad)
+admin.site.register(Licencia)
+admin.site.register(ConversionVacacionesHoras)
+admin.site.register(HorasCompensatoriasSieteDias)
+admin.site.register(CodigoRestablecimiento)
+admin.site.register(RegistroHorasOdoo)
+
+
 
 class UsuarioAdmin(UserAdmin):
     add_form = UsuarioCreationForm
@@ -46,3 +78,15 @@ class FeriadoNacionalAdmin(admin.ModelAdmin):
 class HorasCompensatoriasDescansoAdmin(admin.ModelAdmin):
     list_display = ('usuario', 'registro_origen', 'inicio_descanso', 'fin_descanso', 'horas_compensadas', 'fecha_registro')
     list_filter = ('usuario', 'inicio_descanso')
+
+
+
+
+
+app = apps.get_app_config('gestion_rrhh')
+
+for model in app.get_models():
+    try:
+        admin.site.register(model)
+    except AlreadyRegistered:
+        pass
